5a8a3f1c247d1e703fa01f520396238666d50fe4
huawei <romanec.aleksey@gmail.com>
2024-11-19 21:21:26 +0400

    HW4.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work-4");
-    }
 
+        System.out.println("Задание-1");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Возраст человека = "+age+" он совершеннолетний");
+        } else {
+            System.out.println("Возраст человека = "+age+" он не достиг совершеннолетия, нужно немного подождать».");
+        }
+    }
 }
