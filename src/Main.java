3254f9f27cb39098b5494618a4509f8390c0ee3f
huawei <romanec.aleksey@gmail.com>
2024-11-20 00:35:49 +0400

    HW4.4

--- src/Main.java
+++ src/Main.java
@@ -22,13 +22,37 @@ public class Main {
 
         System.out.println();
         System.out.println("Задание-3");
-        int speedMovements =80;
-        if (speedMovements>60) {
+        int speedMovements = 80;
+        if (speedMovements >= 60) {
             System.out.println("Скорость привышает 60 км/ч придется заплатить штраф");
         } else {
             System.out.println("Скорость не привышает 60 км/ч можно ездить спокойно");
         }
 
+        System.out.println();
+        System.out.println("Задание-4");
 
+        int homan = 65;
+        boolean child = homan >= 2 && homan <= 6;
+        if (child) {
+            System.out.println("Нужно ходить в детский сад");
+        }
+        boolean teenager = homan >= 7 && homan <= 17;
+        if (teenager) {
+            System.out.println("Нужно ходить в школу");
+        }
+
+        boolean youngMan = homan >= 18 && homan <= 24;
+        if (youngMan) {
+            System.out.println("Нужно ходить в университет");
+        }
+        boolean adult = homan >= 25 && homan <= 60;
+        if (adult) {
+            System.out.println("Нужно ходить на работу");
+        }
+        boolean old = homan >= 65;
+        if (old) {
+            System.out.println(" Ты молодец: хорошо учился, усердно работал, а теперь долгожданный отдых, пенсия!!");
+        }
     }
 }
